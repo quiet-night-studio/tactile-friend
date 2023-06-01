edc0fbe6d862353f3b866d4fc3a72b35668104af
ProjectEXT <96099928+ProjectEXT@users.noreply.github.com>
2023-06-01 19:09:00 -0400

    Added Key Animations
    + Relocated items on display

---
+++ scripts/key-A.gd
@@ -0,0 +1,15 @@
+extends Sprite
+
+var default = preload("res://assets/sprites/button/idle-key.png")
+var active = preload("res://assets/sprites/button/active-key.png")
+
+func _input(event):
+#	If the key is being pressed
+	if event is InputEventKey and event.pressed:
+		if event.scancode == KEY_A:
+			texture = active
+	
+	#	If the key is not being pressed
+	if event is InputEventKey and !event.pressed:
+		if event.scancode == KEY_A:
+			texture = default
